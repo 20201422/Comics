a67d097ddc18bf0f9598f9cebabf61d23d36df7c
堃芃24 <2427431300@qq.com>
2023-05-08 19:40:40 +0800

    Initial commit

---
+++ app/src/main/java/com/coop/comics/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coop.comics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
